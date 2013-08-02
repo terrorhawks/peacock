f02e06cf28ea015f774d8196ae4561a8278bbb09
David Clarke <terrorhawks@gmail.com>
2013-08-02 15:17:00 +1000

    more debug

--- lib/peacock/capistrano.rb
+++ lib/peacock/capistrano.rb
@@ -25,7 +25,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     record_sets = record_sets(zone)
 
     staging_prod_ref = find_record(fqdn, record_sets) do |record|
-      record[:resource_records].all? { |r| r[:value]!=current_elb } &&
+      puts record[:resource_records]
+      puts record[:resource_records].all? { |r| r[:value]!=current_elb }
+      record[:resource_records].all? { |r| puts r[:value]; puts current_elb; r[:value]!=current_elb } &&
         record[:type]=='CNAME'
     end
 
